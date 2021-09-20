eb2bc599e95af3c4720c189ac8fa20ed5963eaae
Malu202 <poehlmann.martin@gmail.com>
2021-09-20 14:41:39 +0200

    performance optimization for mobile

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ async function setup() {
             stopped = checkTriggers(drawnData, dataArray, offset);
             triggerTime = performance.now() - triggerTime;
             drawTime = performance.now();
-            //draw(drawnData);
+            //draw2(drawnData);
+            draw(drawnData, offset);
             drawTime = performance.now() - drawTime;
             let a = 0;
         } else {
@@ -102,7 +103,7 @@ function setupBuffers(analyser) {
     dataArray = new Uint8Array(bufferLength);
 };
 
-function draw(drawnData) {
+function draw2(drawnData) {
     context2d.fillStyle = 'rgb(200, 200, 200)';
     context2d.strokeStyle = 'rgb(0, 0, 0)';
 
@@ -144,6 +145,39 @@ function draw(drawnData) {
     context2d.moveTo(0, canvas.height * (1 - trigger2Slider.value));
     context2d.lineTo(canvas.width, canvas.height * (1 - trigger2Slider.value));
     if (trigger2Index != null) { drawTriggerAtIndex(trigger2Index, datapointsPerPixel); }
+    context2d.stroke();
+}
+function draw(drawnData, offset) {
+    context2d.fillStyle = 'rgb(200, 200, 200)';
+    context2d.strokeStyle = 'rgb(0, 0, 0)';
+    //context2d.fillRect(0, 0, canvas.width, canvas.height);
+
+    let datapointsPerPixel = drawnData.length / canvas.width;
+    let pixelShift = Math.round(offset / datapointsPerPixel);
+    if (pixelShift < canvas.width) {
+        let imageData = context2d.getImageData(pixelShift, 0, canvas.width - pixelShift, canvas.height);
+        context2d.putImageData(imageData, 0, 0);
+        context2d.clearRect(canvas.width - pixelShift, 0, pixelShift, canvas.height);
+    } else context2d.fillRect(0, 0, canvas.width, canvas.height);
+
+    let x = canvas.width - pixelShift;
+    context2d.beginPath();
+    for (let iGenau = drawnData.length - offset; iGenau < drawnData.length; iGenau += datapointsPerPixel) {
+        let i = Math.round(iGenau);
+
+        let v = 0;
+        for (let j = 0; j < datapointsPerPixel; j++) {
+            let volume = Math.abs(drawnData[i + j] - 128);
+            if (volume > v) v = volume;
+        }
+        v = v / 128.0;
+        const y = canvas.height - v * canvas.height;
+
+        if (i === 0) context2d.moveTo(x, y);
+        else context2d.lineTo(x, y);
+        x++;
+    }
+    //context2d.lineTo(canvas.width, canvas.height);
     context2d.stroke();
 }
 function drawTriggerAtIndex(index, datapointsPerPixel) {
